6f47032cdc6b4f22cc39e2370bf87c5979412f91
andikarifki <andikarifki03@gmail.com>
2020-10-25 13:19:16 +0800

    first commit

---
+++ coba.php
@@ -0,0 +1,201 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="fontawesome-free/css/all.min.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- Image and text -->
+    <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="img/blud.jpg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+        BLUD
+    </a>
+    </nav>
+  </button>
+  <!-- Nav-item -->
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">About Us <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Commercial</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Residential</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Blog</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Locations</a>
+      </li>
+    </ul>
+  </div>
+  <form class="form-inline my-2 my-lg-0 ml-auto">
+    <div class="icon ml-4">
+      <h5>
+      <button class="btn"><i class="fas fa-search mr-3" data-toogle="tooltip" title="Surat Masuk"></i></button> |
+      <button class="btn"><i class="fab fa-instagram mr-3" data-toogle="tooltip" title="Notifikasi"></i></i></button>
+      <button class="btn"><i class="fab fa-facebook-f mr-3" data-toogle="tooltip" title="Keluar"></i></i></button>
+      <button class="btn"><i class="fab fa-twitter mr-3" data-toogle="tooltip" title="Keluar"></i></i></button>
+      </h5>
+    </div>
+  </div>
+</nav>
+</nav>
+
+<div class="container">
+  <div class="row justify-content-start">
+    <div class="row">
+      <div class="col">
+        <img src="img/latihan.jpg" class="img-fluid" alt="Responsive image">
+      </div>
+        <div class="col">
+          <h1>Local. Everywhere.</h1>
+          <br>
+          <h2>Successful commercial real estate execution blends location with perfect timing and market knowledge.</h2>
+          <br>
+          <h2>Boots on the ground means maximizing your growth potential.</h2>
+          <br>
+          <button type="button" class="btn btn-secondary btn-lg">Schedule Consulation</button>
+        </div>
+      </div>
+    </div>
+  </div>
+      <Br>
+      <Br>
+      <div class="container">
+        <div class="row justify-content-md-center">
+          <div class="col-6">
+            <h1>Using experience and technology to achieve extraordinary results for commercial property investors</h1> 
+          </div>
+        </div>
+      <Br>
+      <Br>
+  <div class="container">
+    <div class="row justify-content-start">
+      <div class="row">
+        <div class="col">
+          <img src="img/guitar.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+          <div class="col">
+            <h1>Commercial</h1>
+              <br>
+                <h2>Fostering Real Value for Commercial Property</h2>
+                  <br>
+                  <h2>
+                  MemmoCRE focuses on the long-term returns of your commercial properties.
+                  Whether you are leasing warehouses, renting office space,
+                  investing in multifamily opportunities, or selling land for development, 
+                  we have the expertise to help you succeed.
+                  </h2>
+              <br>
+            <button type="button" class="btn btn-secondary btn-lg">More Info</button>
+          </div>
+        </div>
+      </div>
+    </div> 
+<br>
+    <div class="container">
+      <div class="row justify-content-start">
+        <div class="row">
+          <div class="col">
+          <br>
+            <h1>Commercial</h1>
+                <h2>Fostering Real Value for Commercial Property</h2>
+                  <br>
+                    <h2>
+                    MemmoCRE focuses on the long-term returns of your commercial properties.
+                    Whether you are leasing warehouses, renting office space,
+                    investing in multifamily opportunities, or selling land for development, 
+                    we have the expertise to help you succeed.
+                    </h2>
+                </div>
+          <div class="col">
+            <img src="img/guitar.jpg" class="img-fluid" alt="Responsive image">
+          </div>
+        </div>
+            <br>
+            <button type="button" class="btn btn-secondary btn-lg">More Info</button>
+          </div>
+        </div>
+      </div>
+    </div>     
+    <br>
+    <div class="container">
+    <div class="p-3 mb-2 bg-secondary text-white">
+      <div class="row justify-content-center">
+          <div class="col-4">
+              <h1>Quotes</h1> 
+                <h2>Our Commitment</h2>
+                  <h3>KW Crosstown is committed to growing interdisciplinary teams of real estate experts in strategic markets across the United States and internationally.
+                  Contact us to learn about joining or starting your own team
+                  </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    <br>
+
+<h1>Blog - Featured Stories</h1>
+
+<div class="row">
+  <div class="col-sm-4">
+    <div class="card">  
+      <div class="card-body">
+          <div class="col">
+            <img src="img/dilon.jpg" class="img-fluid" alt="Responsive image">
+          </div>
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <div class="col">
+              <img src="img/dilon.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <div class="col">
+              <img src="img/dilon.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
